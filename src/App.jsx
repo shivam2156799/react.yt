8fe15247b6b36995b12556dc11df905f73849f1f
Shivam Kuamr <shivamkumar08122006@gmail.com>
2025-10-28 16:05:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent' >
+      <h1 id='child 1'> App </h1>
+      <h2 id='child 2'>Hello</h2>
+    </div>
+  )
+}
+
+export default App
